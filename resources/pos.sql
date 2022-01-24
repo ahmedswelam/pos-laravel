d4b4004c64b9d665cfae05fc2ed468240372aea7
Ahmed Ali Hussein Omar Swelam <62479499+ahmedswelam@users.noreply.github.com>
2022-01-24 16:26:09 +0200

    Add files via upload

---
+++ resources/pos.sql
@@ -0,0 +1,91 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jan 24, 2022 at 02:24 PM
+-- Server version: 8.0.21
+-- PHP Version: 7.4.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+DROP TABLE IF EXISTS `cart`;
+CREATE TABLE IF NOT EXISTS `cart` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `product-name` varchar(50) NOT NULL,
+  `price` int NOT NULL,
+  `quantity` int NOT NULL,
+  `total` int NOT NULL,
+  `product_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `product_id` (`product_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `cart`
+--
+
+INSERT INTO `cart` (`id`, `product-name`, `price`, `quantity`, `total`, `product_id`) VALUES
+(1, 'hp', 1000, 1, 1000, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE IF NOT EXISTS `product` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `pro_name` varchar(45) NOT NULL,
+  `pro_price` int NOT NULL,
+  `pro_quantity` int NOT NULL,
+  `pro_image` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`id`, `pro_name`, `pro_price`, `pro_quantity`, `pro_image`) VALUES
+(2, 'DELL Laptop', 1500, 10, '2.jpg'),
+(3, 'HP Laptop', 1000, 10, '3.jpg'),
+(4, 'MSI Laptop', 2000, 5, '4.jpg'),
+(6, 'HP Laptop', 1000, 10, '1.jpg'),
+(7, 'HP Laptop', 1000, 10, '1.jpg'),
+(8, 'HP Laptop', 1000, 10, '1.jpg'),
+(9, 'HP Laptop', 1000, 10, '1.jpg');
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `cart`
+--
+ALTER TABLE `cart`
+  ADD CONSTRAINT `product_id` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
